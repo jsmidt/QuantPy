6dd30016e473f00e3733d1dc24f0ac1cfe53448a
Joseph Smidt <josephsmidt@gmail.com>
2013-03-03 23:47:00 -0700

    Added Efficient frontier functionality.

--- quantpy/portfolio.py
+++ quantpy/portfolio.py
@@ -1,6 +1,6 @@
 from pandas.io.data import DataReader
-from pylab import legend, xlabel, ylabel, sqrt,ylim, cov, sqrt, mean, std
-from numpy import array, zeros, matrix, ones, shape
+from pylab import legend, xlabel, ylabel, sqrt,ylim, cov, sqrt, mean, std, plot, show
+from numpy import array, zeros, matrix, ones, shape, vectorize, linspace
 from pandas import Series, DataFrame
 from numpy.linalg import inv
 
@@ -68,11 +68,69 @@ class Portfolio:
 
     def cov(self):
         tmp = self.returns()
-       
         tmpl = [] 
         for symbol in tmp.keys():
             tmpl.append(tmp[symbol]) 
         return DataFrame(cov(array(tmpl)),index=tmp.keys(),columns=tmp.keys())
+
+    def sharpe_w(self):
+        V = self.cov()
+        suml = []
+        for symbol in self.asset.keys():
+            suml.append(self.returns()[symbol].sum())
+        f = matrix(suml).T
+        iV = matrix(inv(V))
+        num = iV*f
+        denom = f.T*iV*f
+        w = array(num/denom).flatten()
+        return Series(w,index=self.asset.keys())
+
+    def characteristic_w(self):
+        V = self.cov()
+        suml = []
+        e = matrix(ones(len(self.asset.keys()))).T
+        iV = matrix(inv(V))
+        num = iV*e
+        denom = e.T*iV*e
+        w = array(num/denom).flatten()
+        return Series(w,index=self.asset.keys())
+
+    def efficient_frontier_w(self,fp):
+        wc = self.characteristic_w()
+        wq = self.sharpe_w()
+
+        fc = self.ret_for_w(wc).sum()
+        fq = self.ret_for_w(wq).sum()
+
+        denom = fq-fc
+        w = (fq-fp)*wc + (fp-fc)*wq
+        return Series(w/denom,index=self.asset.keys())
+
+    def efficient_frontier(self,xi=0.01,xf=4,npts=100,scale=10):
+        vret = vectorize(self.ret_for_w)
+        frontier = linspace(xi,xf,npts)
+
+        i = 0
+        rets = zeros(len(frontier))
+        risk = zeros(len(frontier))
+        sharpe = zeros(len(frontier))
+        for f in frontier: 
+            w = self.efficient_frontier_w(f)
+            tmp = self.ret_for_w(w)
+            rets[i] = tmp.sum()*scale 
+            sharpe[i] = mean(tmp)/std(tmp)*sqrt(len(tmp))
+            risk[i] = rets[i]/sharpe[i]
+            i += 1
+        return Series(rets,index=risk), sharpe.max()
+
+    def efficient_frontier_plot(self,xi=0.01,xf=4,npts=100,scale=0.1):
+        eff, m = self.efficient_frontier()
+        plot(array(eff.index),array(eff),'b',label="Efficient Frontier")
+        plot(array(eff.index),m*array(eff.index),'r',label="Max Sharpe Ratio: %6.2g" %m)
+        legend(loc='best',shadow=True, fancybox=True)
+        xlabel('Risk %')
+        ylabel('Return %')
+        show()
 
     def min_var_w_ret(self,ret):
         V = self.cov()
@@ -93,17 +151,5 @@ class Portfolio:
             tmpl.append(tmp[symbol]*w[i])
             i += 1
         return Series(tmpl,index=tmp.keys()).sum()
-
-    def sharpe_w(self):
-        V = self.cov()
-        suml = []
-        for symbol in self.asset.keys():
-            suml.append(self.returns()[symbol].sum())
-        e = matrix(suml).T
-        iV = matrix(inv(V))
-        num = iV*e
-        denom = e.T*iV*e
-        w = array(num/denom).flatten()
-        return Series(w/abs(w.sum()),index=self.asset.keys())
 
 
