55ff18d29c0ca3e50125d9eab61426147c011d00
Bahodir <6ahodir@gmail.com>
2013-05-27 21:56:12 -0400

    Format code to follow PEP8 Style Guide. Support for Python3.

--- quantpy/__init__.py
+++ quantpy/__init__.py
@@ -1,4 +1,4 @@
 from quantpy.portfolio import Portfolio
-from quantpy.event_profiler import *         
-from fundamental_indicators import *
-from technical_indicators import *
+from quantpy.event_profiler import *
+from quantpy.fundamental_indicators import *
+from quantpy.technical_indicators import *
--- quantpy/event_profiler.py
+++ quantpy/event_profiler.py
@@ -5,19 +5,21 @@
 from pylab import errorbar, xlabel, ylabel, show, legend
 from numpy import array, arange
 
-def event_profiler(asset,truth,periods=5):
+
+def event_profiler(asset, truth, periods=5):
     cut = []
-    for i in range(periods,len(asset)-periods):
+    for i in range(periods, len(asset) - periods):
         if truth[i] == 1 and asset[i] > 0:
-            cut.append(asset[i-periods:i+periods]/asset[i])
+            cut.append(asset[i - periods:i + periods] / asset[i])
     return array(cut)
 
-def plot_event_profile(events,name=''):
+
+def plot_event_profile(events, name=''):
     mn = events.mean(axis=0) - 1.0
     st = events.std(axis=0)
-    errorbar(arange(len(mn)),mn,st,label=name)
+    errorbar(arange(len(mn)), mn, st, label=name)
     xlabel('Periods', fontsize=16)
     ylabel('Price Change %', fontsize=16)
     if len(name) > 1:
-        legend(loc='best',shadow=True, fancybox=True)
+        legend(loc='best', shadow=True, fancybox=True)
     show()
