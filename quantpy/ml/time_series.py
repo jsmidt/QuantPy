24276c1ec5459f9047bee99da424e3b27db7498b
Ammar Siddiqui <ammarsiddiqui@Ammars-MacBook-Pro.local>
2023-05-20 16:52:33 -0400

    Added new ml module folder and time series file

---
+++ quantpy/ml/time_series.py
@@ -0,0 +1,15 @@
+'''Models.'''
+
+'''
+Ideas:
+    Regression/Time-series:
+        - ARIMA
+        - ARFIMA
+        - Seasonal Models (SARIMA, Winters)
+        - Spectral Analysis
+        - 
+'''
+
+import pandas as pd
+from statsmodels.tsa.arima.model import ARIMA
+
