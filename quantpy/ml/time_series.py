50bd233b1a507b271df2b5d93c9a99ba03addffd
Ammar Siddiqui <ammarsiddiqui@Ammars-MacBook-Pro.local>
2023-05-22 18:45:46 -0400

    added new time series regression model

--- quantpy/ml/time_series.py
+++ quantpy/ml/time_series.py
@@ -8,8 +8,19 @@ Ideas:
         - Seasonal Models (SARIMA, Winters)
         - Spectral Analysis
         - 
+
+        - Brownian Motion
+        - Martingale + Random Walk
+        - Monte-Carlo
+        - Stochastic Calculus for Finance (Discrete and Continuous)
+        - Implied Volatility
+        - Black Scholes
+        - Merton
+        - Markov
+        - 
 '''
 
 import pandas as pd
 from statsmodels.tsa.arima.model import ARIMA
+
 
